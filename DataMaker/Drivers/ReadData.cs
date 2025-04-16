a2b2dfd49099b1d8f7deb32210d06e5433b180f8
FilipSramek <filip.sid.2004@gmail.com>
2025-04-16 08:13:46 +0200

    Data maker now in repo

---
+++ DataMaker/Drivers/ReadData.cs
@@ -0,0 +1,15 @@
+﻿using Project.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMaker.Drivers
+{
+    class ReadData : SensorData 
+    {
+
+
+    }
+}
---
+++ DataMaker/Form/Form1.cs
@@ -0,0 +1,10 @@
+namespace DataMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
