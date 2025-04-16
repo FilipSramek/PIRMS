1b403f4c1ca13274d88b7c005046988c2c8da5a8
FilipSramek <filip.sid.2004@gmail.com>
2025-04-16 08:07:28 +0200

    Class read DataMaker

---
+++ DataMaker/DataMaker/Drivers/Parser.cs
@@ -0,0 +1,21 @@
+﻿using Project.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMaker.Drivers
+{
+    class Parser
+    {
+        public override string ToString()
+        {
+            return "#Id#Time#Value#";
+        }
+        public static string ToSend(SensorData data)
+        {
+            return $"{data.Id};{data.Time};{data.Value}";
+        }
+    }
+}
---
+++ DataMaker/DataMaker/Drivers/SensorData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Project.Drivers
+{
+    /// <summary>
+    /// Object that holds the data from the sensor
+    /// </summary>
+    public class SensorData
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+
+        
+        public override string ToString()
+        {
+            return $"ID: {Id}, Time: {Time}, Value: {Value}"; //override ToString to return the things that are relevant to me
+        }
+
+        /// <summary>
+        /// Serializes the SensorData object to json file
+        /// </summary>
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        
+        /// <summary>
+        /// Deserializes the data from json file to SensorData object
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public SensorData Deserialize(string data)
+        {
+            return JsonSerializer.Deserialize<SensorData>(data);
+        }
+
+        /// <summary>
+        /// Saves the data to a json file
+        /// </summary>
+        /// <param name="path">.json must be included</param>
+        public void SaveToFile(string path)
+        {
+            File.WriteAllText(path, this.Serialize());
+        }
+    }
+}
